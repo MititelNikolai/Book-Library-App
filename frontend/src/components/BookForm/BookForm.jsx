ae2e47a7c75c814acbecd704708051157aee6b08
MititelNikolai <103959095+MititelNikolai@users.noreply.github.com>
2024-01-14 15:19:08 +0200

    Added Book id

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,7 +1,9 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addBook } from "../../redux/books/actionCreators";
 import "./BookForm.css";
+
 const BookForm = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
@@ -12,6 +14,7 @@ const BookForm = () => {
       const book = {
         title,
         author,
+        id: uuidv4(),
       };
       dispatch(addBook(book));
       setTitle("");
