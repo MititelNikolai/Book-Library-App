720a654688b7cc47df060bdf7720874a5e55ca7f
MititelNikolai <103959095+MititelNikolai@users.noreply.github.com>
2024-01-15 16:48:22 +0200

    Add Book Source

--- frontend/src/utils/createBookWithId.js
+++ frontend/src/utils/createBookWithId.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
-const createBookWithId = (book) => {
-  return { ...book, isFavorite: false, id: uuidv4() };
+const createBookWithId = (book, source) => {
+  return { ...book, source, isFavorite: false, id: uuidv4() };
 };
 
 export default createBookWithId;
